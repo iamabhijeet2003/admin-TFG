c53f7467d2d42cd611cdef50a3f3af1981f16c5b
abhijeet <abhijeet3016@gmail.com>
2024-02-25 22:33:29 +0100

    adding link to products & styling the form

--- src/views/EditProductPage.vue
+++ src/views/EditProductPage.vue
@@ -3,14 +3,16 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <h1>Edit Product</h1>
-          <form @submit.prevent="redirectToEdit">
-            <div class="form-group">
-              <label for="productId">Product ID:</label>
+          <h1 class="h1 text-center mt-5">Edit Product</h1>
+          <form @submit.prevent="redirectToEdit" class="form border border-2 border-success">
+            <div class="form-group col-3 m-4">
+              <label for="productId" class="fw-bolder fs-3">Product ID:</label>
               <input type="number" class="form-control" id="productId" v-model="productId">
             </div>
-            <button type="submit" class="btn btn-primary">Edit Product</button>
+            <button type="submit" class="btn btn-primary m-4">Edit Product</button>
           </form>
+          <small class="small "> Si no sabes el ID del producto, tambien puedes editar el producto en la Pagina de Productos</small>
+          <router-link to="/products" class="btn btn-success btn-md col-12">Ir a la pagina de productos <i class="fa fa-arrow-right"></i> </router-link>
         </div>
       </div>
     </div>
