9c6f8fe687690ab058eb7a012f929a338853e113
abhijeet <abhijeet3016@gmail.com>
2024-03-16 15:57:15 +0100

    adding authentication and states

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,42 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: null,
+    token: null,
+    isAuthenticated: false,
   },
   getters: {
+    isLoggedIn(state) {
+      return !!state.token;
+    },
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setAuthentication(state, isAuthenticated) {
+      state.isAuthenticated = isAuthenticated;
+    },
   },
   actions: {
+    checkAuthentication({ commit }) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        commit("setAuthentication", true);
+      } else {
+        commit("setAuthentication", false);
+      }
+    },
+    logout({ commit }) {
+      localStorage.removeItem('token'); 
+      commit('setToken', null); 
+      commit('setAuthentication', false); 
+      commit('setUser', null); 
+      location.reload();
+    },
   },
   modules: {
   }
