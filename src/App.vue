100bb77e0add2ca673f78315284d628415b81e7f
abhijeet <abhijeet3016@gmail.com>
2024-03-16 15:56:15 +0100

    adding authentication

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
-   <Sidebar />
+  <Sidebar />
   <div :style="{ 'margin-left': sidebarWidth }"></div>
-  
+
   <router-view></router-view>
 </template>
 
 <script>
 import Sidebar from '@/components/sidebar/SideBar'
 import { sidebarWidth } from '@/components/sidebar/state'
-
+import { mapGetters, mapActions } from 'vuex';
 export default {
   name: 'App',
+  async mounted() {
+    //await this.$store.commit('initialiseStore')
+  },
   components: { Sidebar },
   setup() {
     return { sidebarWidth }
   },
+  computed: {
+    ...mapGetters(['isLoggedIn']), // Map isLoggedIn getter
+  },
+  methods: {
+    ...mapActions(['checkAuthentication']), // Map checkAuthentication action
+    ...mapActions(['logout']),
+    handleLogout() {
+      this.logout(); // Call the logout action when the logout button is clicked
+    }
+  },
+  created() {
+    this.checkAuthentication(); // Check authentication status when the component is created
+  },
 }
 </script>
-
-
