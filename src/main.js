e860b7a7cc3ca1fd22d48b419fb99e2e48985a6c
abhijeet <abhijeet3016@gmail.com>
2024-03-16 15:56:43 +0100

    import sweetalert , apexcharts

--- src/main.js
+++ src/main.js
@@ -6,5 +6,14 @@ import store from './store'
 import "bootstrap/dist/js/bootstrap.min";
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import '@fortawesome/fontawesome-free/js/all'
+import VueApexCharts from "vue3-apexcharts";
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(VueApexCharts)
+    .use(VueSweetalert2)
+    .mount('#app')
