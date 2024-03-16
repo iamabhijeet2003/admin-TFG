0740048305f334459c006c89e904c765937add30
abhijeet <abhijeet3016@gmail.com>
2024-03-16 15:59:21 +0100

    adding dashboard charts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,29 @@
 <template>
-    <h1>Hola</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card mt-5 col-6">
+                    <chartView1></chartView1>
+                </div>
+                <div>
+                    <chartView2></chartView2>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    
 </template>
+
+
+<script>
+import chartView1 from '../components/dashboard/charts/ChartView1.vue';
+import chartView2 from '../components/dashboard/charts/ChartView2.vue';
+
+export default{
+    components:{
+        chartView1,
+        chartView2
+    }
+}
+</script>../components/dashboard/charts/ChartView1.vue
