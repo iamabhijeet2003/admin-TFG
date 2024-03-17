bf3b772056437a1aad3a7973b1308c42cef19a16
abhijeet <abhijeet3016@gmail.com>
2024-03-17 22:20:40 +0100

    corrections in style of the modal

--- src/views/ModalAddProduct.vue
+++ src/views/ModalAddProduct.vue
@@ -6,7 +6,7 @@
 
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
             <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Add new Product</h5>
@@ -16,7 +16,7 @@
                 <AddProductForm></AddProductForm>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-secondary mt-2 me-3" data-bs-dismiss="modal">Close</button>
                 
             </div>
             </div>
