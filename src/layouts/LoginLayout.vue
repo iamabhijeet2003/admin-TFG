f754f06bf3325694bbadb7337ee3c1c89844ddd2
abhijeet <abhijeet3016@gmail.com>
2024-04-28 22:58:16 +0200

    add login layout

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,20 @@
+<!-- LoginLayout.vue -->
+<template>
+  <div>
+    <!-- Your login layout HTML structure goes here -->
+    <main>
+      <!-- Login component content will be rendered here -->
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  // No logic needed in this layout component
+}
+</script>
+
+<style>
+/* Login layout styles */
+</style>
