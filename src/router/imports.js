c105945c37158394c375d2a33f30137196a5e201
abhijeet <abhijeet3016@gmail.com>
2024-05-03 23:57:40 +0200

    add lazy load routes in separate file

---
+++ src/router/imports.js
@@ -0,0 +1,15 @@
+export const HomeView = () => import('../views/HomeView.vue');
+export const AddProduct = () => import('../components/AddProduct.vue');
+export const ProductView = () => import('../views/ProductsView.vue');
+export const SingleProductView = () => import('../views/SingleProductView.vue');
+export const CategoriesView = () => import('../views/category/CategoriesView.vue');
+export const SingleCategoryView = () => import('../views/category/SingleCategoryView.vue');
+export const AddProductPage = () => import('../views/AddProductPage.vue');
+export const AddCategoryPage = () => import('../views/category/AddCategoryPage.vue');
+export const EditProduct = () => import('../views/EditProduct.vue');
+export const EditProductPage = () => import('../views/EditProductPage.vue');
+export const InventoryShow = () => import('../views/inventory/InventoryShow.vue');
+export const Login = () => import("../views/LoginView.vue");
+export const OrderView = () => import("../views/orders/OrdersView.vue");
+export const UserView = () => import('../views/user/UsersView.vue');
+export const EditCategory = () => import('@/components/forms/EditCategoryForm.vue');
