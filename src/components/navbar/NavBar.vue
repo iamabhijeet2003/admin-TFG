aac29bd73fa97870a8334f22bb27b8c4b8b604a7
abhijeet <abhijeet3016@gmail.com>
2024-05-03 23:38:52 +0200

    add vuex state and logout logic

--- src/components/navbar/NavBar.vue
+++ src/components/navbar/NavBar.vue
@@ -1,54 +1,88 @@
 <template>
-    <div>
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown"> 
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/logout">Logout</a>
-              </li>
-            </ul>
-          </div>
+  <div>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown link
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item" style="cursor: pointer;">
+              <KeepAlive>
+                <router-link v-if="!isLoggedIn" to="/login" class="nav-link text-white me-2">
+
+
+
+
+                  <span class="text-black fw-bold">Login</span>
+                </router-link>
+                <div v-else @click="handleLogout" class="nav-link text-white me-2">
+
+
+                  <span class="text-black fw-bold">Logout</span>
+                </div>
+              </KeepAlive>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
-  </template>
-  
+      </div>
+    </nav>
+  </div>
+</template>
 
-  <script>
 
+<script>
+import { mapGetters, mapActions } from 'vuex'; // Import mapGetters from Vuex
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters(['isLoggedIn']), // Map isLoggedIn getter
+  },
+  methods: {
+    ...mapActions(['checkAuthentication']), // Map checkAuthentication action
+    ...mapActions(['logout']),
+    handleLogout() {
+      this.logout(); // Call the logout action when the logout button is clicked
+    },
+  }
+
+}
 </script>
-  <style scoped>
-  :root {
-    --navbar-bg-color: #500af3;
-    --navbar-item-hover: #509adf;
-    --navbar-item-active: #220b85;
-  }
-  .navbar{
-    background-color: #cec3e6;
-  }
-  </style>
-  
+<style scoped>
+:root {
+  --navbar-bg-color: #500af3;
+  --navbar-item-hover: #509adf;
+  --navbar-item-active: #220b85;
+}
+
+.navbar {
+  background-color: #cec3e6;
+}
+</style>
