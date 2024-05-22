98f4e447d6d2e525bfcc527a07606b32e60c0692
abhijeet <abhijeet3016@gmail.com>
2024-05-23 00:37:09 +0200

    modify positions of charts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                
+                <div>
+                    <chartView3></chartView3>
+                </div>
                 <div class="card mt-5 col-6">
                     <chartView1></chartView1>
                 </div>
                 <div>
-                    <chartView2></chartView2>
+                    <TotalSales></TotalSales>
                 </div>
                 <div>
-                    <chartView3></chartView3>
+                    <chartView2></chartView2>
                 </div>
+                
                 <div class="col-12">
                     <chartView4></chartView4>
                 </div>
@@ -32,14 +35,15 @@ import chartView2 from '../components/dashboard/charts/ChartView2.vue';
 import chartView3 from '../components/dashboard/charts/ChartView3Sales.vue';
 import chartView4 from '../components/dashboard/charts/ChartView4.vue';
 import chartView5 from '../components/dashboard/charts/ChartView5.vue';
-
+import TotalSales from '../components/dashboard/TotalSales.vue'
 export default{
     components:{
         chartView1,
         chartView2,
         chartView3,
         chartView4,
-        chartView5
+        chartView5,
+        TotalSales
     }
 }
 </script>../components/dashboard/charts/ChartView1.vue
