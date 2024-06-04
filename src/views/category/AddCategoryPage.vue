2ef6f0663be0ef265d13d7a1bd01499a5b30dfaf
abhijeet <abhijeet3016@gmail.com>
2024-06-04 19:02:19 +0200

    correction sidebar width

--- src/views/category/AddCategoryPage.vue
+++ src/views/category/AddCategoryPage.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        <h1 class="h1 text-center">Add a new category</h1>
+        <h1 class="h1 text-center" style="margin-left: 250px;">Add a new category</h1>
         <div class="row">
             <div class="col">
                 <AddCategoryForm></AddCategoryForm>
             </div>
-            <div class="text-end">
-                <router-link to="/categories" class="btn btn-lg btn-success">Go back to category</router-link>
+            <div class="text-end me-5">
+                <router-link to="/categories" class="btn btn-lg btn-success me-5">Go back to category</router-link>
             </div>
         </div>
     </div>
